8ba6cf5400e673a18218d24a904f0b83b69d4b14
Shrutika Rahatekar <53502821+Shrutika432@users.noreply.github.com>
2021-03-25 11:01:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/integrategoogleapilogin/Login.java
@@ -0,0 +1,77 @@
+package com.example.integrategoogleapilogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class Login extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
+    Button logout;
+    TextView name,email,id;
+    ImageView pic;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        name=findViewById(R.id.name);
+        email=findViewById(R.id.email);
+        id=findViewById(R.id.eid);
+        logout=findViewById(R.id.button);
+        pic=findViewById(R.id.imageView);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+            name.setText("Name: " +name);
+            email.setText("Email: " +email);
+            id.setText("ID: " +id);
+            Glide.with(this).load(String.valueOf(personPhoto)).into(pic);
+
+        }
+
+    }
+
+    private void signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(Login.this, "Signout successful", Toast.LENGTH_SHORT).show();
+                        // ...
+                        finish();
+                    }
+                });
+    }
+
+}
